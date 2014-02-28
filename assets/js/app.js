b053daf96cf54d202b89f4a05db34869d34f45fd
khay <khayusaki@gmail.com>
2014-02-28 15:03:12 +0630

    Added social links for team and update header. Using media queries now.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,28 @@
 $(function() {	
     $('.nav ul li a').bind('click',function(event){    	
+    	event.preventDefault();
         var $anchor = $(this);
 
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 119,
         }, 1000, 'easeOutQuart');        
+    });
+
+    /* Menu Responsive Toggler */
+    var toggler = $('#toggler');
+        menu        = $('.nav ul');
+        menuHeight  = menu.height();
+
+    $(toggler).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
     });
 	
 });
